033139a52cb392fe0066c1ee47f2e985cae6467a
vthangamani <134281152+vthangamani@users.noreply.github.com>
2026-07-13 09:41:43 +0530

    ACP1.java

---
+++ AddnumbersACP.java
@@ -0,0 +1,14 @@
+public class AddNumbers 
+{
+    public static void main(String[] args) {
+        // Step 1: Initialize 2 integers
+        int x = 10;
+        int y = 20;
+
+        // Step 2: Initialize z as the sum of x and y
+        int z = x + y;
+
+        // Step 3: Display the value of z
+        System.out.println("The sum is: " + z);
+    }
+}
